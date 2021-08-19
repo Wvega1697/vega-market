c8633fe058d32498d7046e8f7b4e3bca8fc10d3b
WVEGA1697 <williamsv1697@gmail.com>
2021-08-18 22:14:27 -0600

    HolaMundo
    Primer commit con la config inicial y una clase para imprimir Hola Mundo

---
+++ src/main/java/com/wvega/market/VegaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.wvega.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvega/market/VegaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvega.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
