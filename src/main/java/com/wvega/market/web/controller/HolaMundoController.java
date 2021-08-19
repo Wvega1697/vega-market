97a317913cf4fcdda6c65114141e364fa89e7335
WVEGA1697 <williamsv1697@gmail.com>
2021-08-19 10:45:44 -0600

    Estructura basada en Dominio
    Se agregaron los paquetes para la estructua y 2 archivos properties

--- src/main/java/com/wvega/market/web/controller/HolaMundoController.java
+++ src/main/java/com/wvega/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.wvega.market;
+package com.wvega.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
