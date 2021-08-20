003ddf0c2d53fdfa9c69c5ee7b2201573458985d
WVEGA1697 <williamsv1697@gmail.com>
2021-08-20 07:57:40 -0600

    Perrito Hola Mundo
    🐶

--- src/main/java/com/wvega/market/web/controller/HolaMundoController.java
+++ src/main/java/com/wvega/market/web/controller/HolaMundoController.java
@@ -10,7 +10,18 @@ public class HolaMundoController {
 
     @GetMapping("/hola")
     public String saludar(){
-        return "Hola Mundo de Spring!! 🍃";
+        return "                       __\n" +
+                "                     .'  '.\n" +
+                "                 _.-'/  |  \\\n" +
+                "    ,        _.-\"  ,|  /  0 `-.\n" +
+                "    |\\    .-\"       `--\"\"-.__.'=====================-,\n" +
+                "    \\ '-'`        .___.--._)=========================|\n" +
+                "     \\            .'      |                          |\n" +
+                "      |     /,_.-'        |        HOLA MUNDO        |\n" +
+                "    _/   _.'(             |           EN             |\n" +
+                "   /  ,-' \\  \\            |         SPRING           |\n" +
+                "   \\  \\    `-'            |                          |\n" +
+                "    `-'                   '--------------------------'";
     }
 
     @GetMapping("/adios")
